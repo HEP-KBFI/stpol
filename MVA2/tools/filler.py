805d06480ee57412b95442f1ad76517c8b21a2d5
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-08-05 19:02:56 +0200

    Change filler.py default ouput directory.

--- MVA2/tools/filler.py
+++ MVA2/tools/filler.py
@@ -37,12 +37,9 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('mva', help = 'output .root of mva trainer')
 	parser.add_argument('trees', nargs = '*', help = 'list of input root files')
-	parser.add_argument('-o', '--odir', default='.', help = 'output dir')
+	parser.add_argument('-o', '--odir', default=None, help = 'output dir')
 	parser.add_argument('-s', '--suf', default='mva', help = 'filename suffix')
 	args = parser.parse_args()
-
-	if not os.path.isdir(args.odir):
-		os.makedirs(args.odir)
 
 	mvas = mvalib.fill.read_mvas(args.mva)
 	reader = mvalib.fill.MVAReader(mvas.items()[0][1]['varlist'])
@@ -51,8 +48,12 @@ if __name__ == '__main__':
 		reader.book_method(name, mvameta)
 
 	for tree_ifname in args.trees:
+		odir = os.path.dirname(tree_ifname) if args.odir is None else args.odir
+		if not os.path.isdir(odir):
+			os.makedirs(odir)
+		
 		sample_name = mvalib.utils.get_sample_name(tree_ifname)
-		tree_ofname = os.path.join(args.odir, '{0}_{1}.root'.format(sample_name, args.suf))
+		tree_ofname = os.path.join(odir, '{0}_{1}.root'.format(sample_name, args.suf))
 		[tree_ifname,tree_ofname] = map(os.path.abspath, [tree_ifname,tree_ofname])
 		print tree_ifname,'>',tree_ofname
 		fill_tree(reader, tree_ifname, tree_ofname)
