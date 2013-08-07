dda3789f1736a7da5a13d79b6a5c8b831448f891
Morten Piibeleht <morten.piibeleht@cern.ch>
2013-08-07 15:43:00 +0200

    Add a script to fill MVAs to the same ROOT file.

---
+++ MVA2/tools/fill_same.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Script that fills the MVA values to the same ROOT file."""
+
+import array
+import pprint
+import os, os.path, shutil
+import ROOT; ROOT.PyConfig.IgnoreCommandLineOptions = True
+import mvalib.fill
+import mvalib.utils
+
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('mva', help = 'output .root of mva trainer')
+	parser.add_argument('trees', nargs = '*', help = 'list of input root files')
+	parser.add_argument('-o', '--odir', default=None, help = 'output dir')
+	parser.add_argument('-s', '--suf', default='mva', help = 'filename suffix')
+	args = parser.parse_args()
+
+	mvas = mvalib.fill.read_mvas(args.mva)
+	reader = mvalib.fill.MVAReader(mvas.items()[0][1]['varlist'])
+
+	for name,mvameta in mvas.items():
+		reader.book_method(name, mvameta)
+
+	for tree_ifname in args.trees:
+		mvalib.fill.fill_tree(reader, tree_ifname, None)
