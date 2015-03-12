88e82ac5a0470f4e4c536b0539270e21d7fcea09
andresti <andres.tiko@cern.ch>
2015-03-12 17:01:46 +0200

    Update theta run conf

--- src/fraction_fit/runtheta.sh
+++ src/fraction_fit/runtheta.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 echo "running theta wrapper";
-echo $PYTHONPATH
-DYLD_LIBRARY_PATH=../../theta/lib:$DYLD_LIBRARY_PATH ../../theta/utils2/theta-auto.py $@;
+#echo $PYTHONPATH
+#shopt -s expand_aliases
+#source ~/.bashrc
+#shopt -s expand_aliases
+#echo `which python`
+#python -v
+#DYLD_LIBRARY_PATH=../../theta/lib:$DYLD_LIBRARY_PATH ../../theta/utils2/theta-auto.py $@;
+#echo $LD_LIBRARY_PATH
+LD_LIBRARY_PATH=/scratch/andres/theta/lib:$LD_LIBRARY_PATH /scratch/andres/theta/utils2/theta-auto.py $@;
 echo "theta wrapper is done";
 exit 0;
 
