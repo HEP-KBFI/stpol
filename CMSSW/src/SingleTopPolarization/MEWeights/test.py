773abb281495f28657bb048c91a53296d50f74b3
andresti <andres.tiko@cern.ch>
2015-03-12 15:28:25 +0200

    Use step1 output for testing

--- CMSSW/src/SingleTopPolarization/MEWeights/test.py
+++ CMSSW/src/SingleTopPolarization/MEWeights/test.py
@@ -9,7 +9,9 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'root://xrootd.unl.edu//store/mc/Summer12_DR53X/TToLeptons_t-channel_scaleup_8TeV-powheg-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/022CADC1-A0D9-E111-9916-0030487FA4CB.root'
+        #'root://xrootd.unl.edu//store/mc/Summer12_DR53X/TToLeptons_t-channel_scaleup_8TeV-powheg-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/022CADC1-A0D9-E111-9916-0030487FA4CB.root'
+        'file:/hdfs/cms/store/user/jpata/TToLeptons_t-channel_8TeV-powheg-tauola/Sep8_newjec_metsystshift_hermeticproj/b5f3018263c1ef499fcfff38dc3a7740/output_noSkim_100_1_5j9.root'
+        
     )
 )
 
