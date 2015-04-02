221f13f931dbe2acb62338062e41ed97652484e5
andresti <andres.tiko@cern.ch>
2015-04-02 14:04:35 +0300

    Remove old BDTs

--- src/skim/merge.sh
+++ src/skim/merge.sh
@@ -15,13 +15,13 @@ mvapath=/home/andres/single_top/stpol_pdf/src/mva
 (
     source /home/software/root_v_5_34_21/bin/thisroot.sh
     python $mvapath/adder.py bdt_sig_bg_old $mvapath/weights/stpol_bdt_sig_bg_lepton_flavour.weights.xml $FILE_NAMES
-    python $mvapath/adder.py bdt_sig_bg_before_reproc $mvapath/weights/stpol_bdt_sig_bg_mixed_lepton_flavour.weights.xml $FILE_NAMES 
+    #python $mvapath/adder.py bdt_sig_bg_before_reproc $mvapath/weights/stpol_bdt_sig_bg_mixed_lepton_flavour.weights.xml $FILE_NAMES 
     #python $mvapath/adder.py bdt_sig_bg $mvapath/weights/stpol_bdt_sig_bg_mixed_new_lepton_flavour.weights.xml $FILE_NAMES 
     #python $mvapath/adder.py bdt_sig_bg $mvapath/weights/stpol_bdt_sig_bg_mixed_deltaR_lepton_flavour.weights.xml $FILE_NAMES
     python $mvapath/adder.py bdt_sig_bg $mvapath/weights/stpol_bdt_sig_bg_mixed_fullData_lepton_flavour.weights.xml $FILE_NAMES 
     #python $mvapath/adder.py bdt_sig_bg_withC $mvapath/weights/stpol_bdt_sig_bg_mixed_fullData_withC_lepton_flavour.weights.xml $FILE_NAMES  
     #python $mvapath/adder.py bdt_sig_bg_nonmixed $mvapath/weights/stpol_bdt_sig_bg_reproc_lepton_flavour.weights.xml $FILE_NAMES 
-    python $mvapath/qcd_mva_adder.py bdt_qcd_reproc anti_QCD_MVA_28Nov_qcdBDT $FILE_NAMES
+    #python $mvapath/qcd_mva_adder.py bdt_qcd_reproc anti_QCD_MVA_28Nov_qcdBDT $FILE_NAMES
     #python $mvapath/qcd_mva_adder.py bdt_qcd_old anti_QCD_MVA_07_04_final2 $FILE_NAMES
     #python $mvapath/qcd_mva_adder.py bdt_qcd_mixed  anti_QCD_MVA_28Nov_qcdBDT_mixed_ $FILE_NAMES
     python $mvapath/qcd_mva_adder.py bdt_qcd anti_QCD_MVA_27Jan_fullData_qcdBDT $FILE_NAMES
