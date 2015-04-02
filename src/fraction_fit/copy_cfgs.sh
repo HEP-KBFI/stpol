553f96d62ca98f2bcdfbae1121bd8dbcea894fe5
andresti <andres.tiko@cern.ch>
2015-04-02 13:51:50 +0300

    Wjets pt weight uncertainty to fit

--- src/fraction_fit/copy_cfgs.sh
+++ src/fraction_fit/copy_cfgs.sh
@@ -2,7 +2,7 @@
 inf=$1
 od=`dirname $inf`
 
-for syst in jes jer tchan_scale wzjets_scale ttjets_scale mass met pu btag_bc btag_l lepton_id lepton_iso lepton_trigger ttjets_matching wzjets_matching wjets_shape top_weight wjets_flavour_heavy wjets_flavour_light qcd_antiiso qcd_yield twchan schan dyjets diboson lepton_weight pdf tchan_qscale_me_weight ttjets_qscale_me_weight wzjets_qscale_me_weight #wjets_light_matching wjets_light_scale
+for syst in jes jer tchan_scale wzjets_scale ttjets_scale mass met pu btag_bc btag_l lepton_id lepton_iso lepton_trigger ttjets_matching wzjets_matching wjets_shape top_weight wjets_flavour_heavy wjets_flavour_light qcd_antiiso qcd_yield twchan schan dyjets diboson lepton_weight pdf tchan_qscale_me_weight ttjets_qscale_me_weight wzjets_qscale_me_weight wjets_pt_weight #wjets_light_matching wjets_light_scale
 do
     for dir in up down
     do
