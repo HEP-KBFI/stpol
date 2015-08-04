aff569b55e1548db3f1e09e779914381d8767318
andresti <andres.tiko@cern.ch>
2015-08-04 16:10:04 +0300

    Switch PDFs to 20 bins for plotting

--- src/pdf_uncertainties/variables.py
+++ src/pdf_uncertainties/variables.py
@@ -2,9 +2,12 @@ variables = ["bdt_sig_bg", "cos_theta", "pdfweight"]
 variables.extend(["scale", "id1", "id2", "x1", "x2"])
 
 ranges = {}
-ranges["bdt_sig_bg"] = (30, -1, 1)
-ranges["cos_theta"] = (48, -1, 1)
-ranges["cos_theta_lj_gen"] = (24, -1, 1)
+#ranges["bdt_sig_bg"] = (30, -1, 1)
+#ranges["cos_theta"] = (48, -1, 1)
+#ranges["cos_theta_lj_gen"] = (24, -1, 1)
+ranges["bdt_sig_bg"] = (20, -1, 1)
+ranges["cos_theta"] = (20, -1, 1)
+ranges["cos_theta_lj_gen"] = (20, -1, 1)
 ranges["pdfweight"] = (100, -200, 200)
 ranges["scale"] = (100, 150, 500)
 ranges["id1"] = (13, -6.5, 6.5)
